6209b78d6d9cfeddde44df1989fe9105852d7127
hytkgami <gagagatrump@gmail.com>
2015-08-10 00:42:49 +0900

    マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809093408) do
+ActiveRecord::Schema.define(version: 20150809145359) do
 
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at",             null: false
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20150809093408) do
     t.integer  "price"
     t.string   "message"
     t.string   "address"
+    t.string   "filename"
+    t.binary   "image"
   end
 
   add_index "rooms", ["user_id"], name: "index_rooms_on_user_id"
@@ -54,6 +56,7 @@ ActiveRecord::Schema.define(version: 20150809093408) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name",                   default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
