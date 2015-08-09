4ba6c4dfb77259f4446ca51db0483d121432b272
yuki <b1434514@planet.kanazawa-it.ac.jp>
2015-08-10 00:42:48 +0900

    デザインを一部修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809093408) do
+ActiveRecord::Schema.define(version: 20150809121234) do
 
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at",             null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150809093408) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name",                   default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
