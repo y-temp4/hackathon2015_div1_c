0a20ba6dc319a64caa8d60bd5d8cecb1e5a8f631
SShayashi <SShayashi0208@gmail.com>
2015-08-09 21:13:39 +0900

    アクションの編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,14 +1,26 @@
 class RoomsController < ApplicationController
   def index
+    @rooms = Room.all
   end
 
   def show
+
   end
 
   def new
+    @room = Room.new    
+    @room.user_id = current_user.id
   end
 
   def create
+    @room = Room.new(params[:room])
+    if @room.save
+      session[:room] = @room.id
+      redirect_to @room, notice: "予約が完了しました。"
+    else
+      flash.notice = "予約に失敗しました。"
+      render "new"
+    end
   end
 
   def edit
