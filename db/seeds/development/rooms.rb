3773301041429d3b639ac838eb5b030a4c62bae2
hytkgami <gagagatrump@gmail.com>
2015-08-10 11:50:05 +0900

    seedファイルの作成

---
+++ db/seeds/development/rooms.rb
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+messages = %w(hoge fuga piyo bar foo)
+address = %w(東京都 埼玉 神奈川 千葉 山梨)
+
+0.upto(9) do |n|
+  Room.create(
+                 user_id: rand(10) + 1,
+                 title: "Room No #{n}",
+                 message: messages[n%5],
+                 address: address[n%5],
+                 amount: n+2,
+                 price: 100*n
+                )
+end
